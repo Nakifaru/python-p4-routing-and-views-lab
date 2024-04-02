92fdc46608df6f8769126e40473a0bca1ec5a44c
Michael <michaelmwendwamumo@gmail.com>
2024-04-02 12:55:32 +0300

    Passing 6 tests

--- server/app.py
+++ server/app.py
@@ -6,3 +6,21 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+    print(parameter)
+    return f'{parameter}'
+
+@app.route('/count/<int:number>')
+def count(number):
+    for x in range(number):
+        return f'{x}\n'
+
+@app.route('/math/<num1>/<operation>/<num2>')
+def math(num1, operation, num2):
+    return f'{num1} {operation} {num2}'
